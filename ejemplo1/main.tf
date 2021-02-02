14eb9cbfaf4f8c6fd0b3fa54b79e9305ac2b2cac
Ubuntu <ubuntu@ip-172-31-2-139.eu-west-1.compute.internal>
2021-02-02 18:53:33 +0000

    ejemplo docker completo

--- ejemplo1/main.tf
+++ ejemplo1/main.tf
@@ -15,5 +15,16 @@ resource "docker_image" "imagen-ubuntu" {
 resource "docker_container" "contenedor-ubuntu" {
   name  = "mi_contenedor_ubuntu"
   image = docker_image.imagen-ubuntu.latest
+ # En  el apply que se quede corriendo 10 min.
+  command = ["bash","-c","sleep 600"]
+  dynamic "volumes" {
+    for_each = var.volumenes
+    content {
+#        volume_name = contains(keys(volumes), "volume_name") ? volumes.value[volume_name] : null
+        volume_name    = volumes.value["volume_name"]
+        host_path      = volumes.value["host_path"]
+        container_path = volumes.value["container_path"]
+    }
+  }
+  
 }
-
--- ejemplo1/variables.tf
+++ ejemplo1/variables.tf
@@ -3,3 +3,20 @@ variable "nombre_imagen" {
     type        = string
     default     = "ubuntu:latest"
 }
+
+variable "volumenes" {
+    description = "Volumenes que vamos a montar en el contenedor"
+    type        = list(map(string))
+    default     = [
+        {
+            volume_name = "vol_ivan1"
+            host_path      = "/home/ubuntu/environment/cursoTerraform"
+            container_path = "/cursoTerraform"
+        },
+        {
+            volume_name = "vol_ivan2"
+            container_path = "/ivan"
+            host_path      = "/home/ubuntu/environment/ivan"
+        }
+    ]
+}
